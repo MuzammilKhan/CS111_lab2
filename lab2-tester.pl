af4ed61fe2c3aa2084c68cb2df2227f77331d3c9
Andy Shih <andyshih12@hotmail.com>
2015-11-11 23:35:16 -0800

    small typo

--- lab2-tester.pl
+++ lab2-tester.pl
@@ -169,7 +169,7 @@ close FOO;
     
     # 18
     [ #Test for device busy
-      '(./osprdaccess -r 4 -l -d 5) &' .
+      '(./osprdaccess -r 4 -l -d 5) & ' .
       '(echo bar | ./osprdaccess -w -L)',
       "ioctl OSPRDIOCTRYACQUIRE: Device or resource busy "
     ],
@@ -177,7 +177,7 @@ close FOO;
     # 19
     [ #Test for deadlock
       '(echo foo | ./osprdaccess -w -l /dev/osprda /dev/osprda)',
-      "ioctl OSPRDIOCQACQUIRE: Resource deadlock avoided"
+      "ioctl OSPRDIOCACQUIRE: Resource deadlock avoided"
     ],
 
     #sanity check
